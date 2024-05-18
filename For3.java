9c7f3fe35af43c6128633c08be6c40b477301aa9
Kashan Memon <kashanmemon16@gmail.com>
2024-05-18 23:37:58 +0500

    Add files via upload
    Java program uploaded by kashan memon
    
    Signed-off-by: Kashan Memon <kashanmemon16@gmail.com>

---
+++ For3.java
@@ -0,0 +1,8 @@
+public class For3 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 11; i++ ){
+            System.out.println("\n\n\n\n\n"+i+"-");
+        }
+    }
+    
+}
